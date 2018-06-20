6ea377c3b5712c9bcd3f995e7c0dfe6817aad6f3
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-06-20 16:10:41 +0200

    simplifying makefiles

---
+++ source/make/common.mk
@@ -0,0 +1,32 @@
+# compilers and flags
+
+use_torc?=1
+
+CC := mpicc
+LD := mpicc
+
+ifeq ($(use_torc),1)
+	CC = mpicc
+	CFLAGS += -D_USE_TORC_=1 `torc_cflags`
+	LDLIBS += `torc_libs`
+endif
+
+CFLAGS += -O3 -std=c99
+CFLAGS += -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
+CFLAGS += -Wall -Wno-unused-function
+CFLAGS += `gsl-config --cflags`
+
+LDLIBS += `gsl-config --libs`  -lm -lpthread
+
+
+COMPILE.c = $(CC) $(CFLAGS) -c -o $@
+LINK.o    = $(LD) $(LDFLAGS) -o $@
+
+# rules
+
+%.o: %.c
+	@mkdir -p "$(@D)"
+	$(COMPILE.c) $<
+
+%.a:; ar rcs $@ $^
+
