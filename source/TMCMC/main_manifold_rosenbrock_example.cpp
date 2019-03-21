93c72a40ead5a47a45c02a6dfd3b7e779fc5d8ce
wadaniel <wadaniel@ethz.ch>
2019-03-21 14:19:59 +0100

    moved old c code

---
+++ source/TMCMC/main_manifold_rosenbrock_example.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "fitfun.hpp"
+#include "rosenbrock.hpp"
+#include "engine_tmcmc.hpp"
+
+using namespace tmcmc;
+using namespace fitfun;
+
+int main(int argc, char *argv[])
+{
+    Fitfun rosenbrock = Fitfun(*f_minusRosenbrock);
+
+    TmcmcEngine engine(&rosenbrock, Manifold);
+    engine.run();
+    return 0;
+}
