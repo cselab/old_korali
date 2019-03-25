9b0ed5e9c4702b257dacc745c39a963c3b8c74aa
wadaniel <wadaniel@ethz.ch>
2019-03-25 14:31:50 +0100

    added gitignore for TMCMC folder

--- source/likelihoodsxx/gaussian.hpp
+++ source/likelihoodsxx/gaussian.hpp
@@ -12,6 +12,14 @@ double f_gaus(const double *x, int N)
 {
     double s = 0.;
     for (int i = 0; i < N; ++i) s += x[i]*x[i];
+
+    /*
+    printf("N:%d\n",N);
+    printf("s:%f\n",s);
+    printf("x0:%f\n",x[0]);
+    printf("x1:%f\n",x[1]);
+    */
+
     return pow(sqrt(2*M_PI),-0.5*N)*exp(-0.5*s);
 }
 
@@ -23,6 +31,8 @@ gsl_vector* gradX_gaus(const double *x, int N)
 
     for (int i = 0; i < N; ++i) gsl_vector_set(grad,i,-x[i]*f);
 
+    //printf("grad0:%f\n", gsl_vector_get(grad,0));
+    //printf("grad1:%f\n", gsl_vector_get(grad,1));
     return grad;
 }
 
