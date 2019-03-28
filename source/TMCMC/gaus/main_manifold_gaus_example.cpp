6177d3ebc8392fe169f1ac94d0180bf234dd2b49
wadaniel <wadaniel@ethz.ch>
2019-03-28 09:04:27 +0100

    fixed expectedloglike, cleaned directory, moved code

---
+++ source/TMCMC/gaus/main_manifold_gaus_example.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#include "fitfun.hpp"
+#include "gaussian.hpp"
+#include "engine_tmcmc.hpp"
+
+using namespace tmcmc;
+using namespace fitfun;
+
+int main(int argc, char *argv[])
+{
+
+    int N = 5;
+    double LB = -10;
+    double UB = 10;
+
+    Fitfun gaus = Fitfun(*f_gaus, *gradX_gaus, *hessX_gaus);
+
+    TmcmcEngine engine(&gaus, Manifold, "gaus_tmcmc.par", "gaus_priors.par");
+    
+    engine.run();
+    
+    double *mean = engine.getNewMean();
+    double **SS = engine.getNewSampleCov();
+
+    double err = 0;
+    for(int i = 0; i < N; ++i) {
+        err += fabs(mean[i])/N;
+        for(int j = 0; j < N; ++j) {
+            if (i == j) err += fabs(1 - SS[i][j])/(N*N);
+            else err += fabs(SS[i][j])/(N*N);
+        }
+    }
+
+    delete mean;
+    for(int i = 0; i < 5; ++i) delete SS[i];
+    delete SS;
+
+    double explogevidence = -0.5*N*log(2.0*M_PI) - N*log(UB-LB);
+    printf("err: %f\n",err);
+    printf("expected log evidence: %f\n", explogevidence);
+    
+    return 0;
+}
---
+++ source/TMCMC/rosenbrock/main_gp_rosenbrock_example.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+#include <libgp/libgp/include/core/gp.h>
+#include <libgp/libgp/include/core/gp_utils.h>
+#include <libgp/libgp/include/core/rprop.h>
+
+#include "fitfun.hpp"
+#include "rosenbrock.hpp"
+#include "engine_tmcmc.hpp"
+
+using namespace libgp;
+using namespace tmcmc;
+using namespace fitfun;
+
+void testGp(GaussianProcess& gp, double low, double up, size_t N);
+
+int main(int argc, char *argv[])
+{    
+    double low = -3;
+    double up  = 3;
+
+    size_t nTraining = 100;
+    size_t nTest     = 100;
+    size_t nOptSteps = 5000;
+    
+    // initialize Gaussian process for 2-D input using the squared exponential 
+    printf("initializing Gaussian process .. \n");
+    GaussianProcess gp(2, "CovSum ( CovSEiso, CovNoise)");
+    // initialize hyper parameter vector
+    Eigen::VectorXd params(gp.covf().get_param_dim()); // 3 (2 for exponential + 1 for noise)
+    params << 0.0, 0.0, -1.0;
+    // set parameters of covariance function
+    gp.covf().set_loghyper(params);
+
+    // add training patterns
+    printf("add training data to Gp ( N = %zu )..  \n", nTraining);
+    double y;
+    double x[2];
+    for(size_t i = 0; i < nTraining; ++i) {
+      x[0] = drand48()*(up-low)+low;
+      x[1] = drand48()*(up-low)+low;
+      y = f_minusRosenbrock(x, 2);
+      gp.add_pattern(x, y);
+    }
+    printf("loglike before maximization %f \n", gp.log_likelihood());
+    testGp(gp, low, up, nTest);
+
+    // maximize with RProp
+    RProp rprop;
+    rprop.init(1e-4);
+    printf("maximize hyper params of Gp ( max iterations: %zu) .. \n", nOptSteps);
+    rprop.maximize(&gp, nOptSteps, false);
+    printf("loglike after maximization %f \n", gp.log_likelihood());
+    testGp(gp, low, up, nTest);
+
+    // init fitfun
+    auto gplambda = [&gp] (const double *theta, int N) { return gp.f(theta); };
+    Fitfun rosenbrock = Fitfun(gplambda);
+
+    //tmcmcm
+    printf("run Tmcmcmc.. \n");
+    TmcmcEngine engine(&rosenbrock, Standard, "tmcmc_rosenbrock.par", "priors_rosenbrock.par");
+    engine.run();
+    
+    //exit
+    printf("exit succefull \n");
+    return 0;
+}
+
+void testGp(GaussianProcess& gp, double low, double up, size_t N){
+    printf("calculating MSE ( N = %zu ) .. \n", N);
+    double max = -std::numeric_limits<double>::max();
+    double e, se = 0;
+    double x[2] , xmax[2];
+    for(size_t i = 0; i < N; ++i) {
+        x[0] = drand48()*(up-low)+low;
+        x[1] = drand48()*(up-low)+low;
+        
+        e = pow(f_minusRosenbrock(x,2) - gp.f(x), 2);
+        if(e > max) { max = e; xmax[0] = x[0]; xmax[1] = x[1]; }
+        
+        se += pow(f_minusRosenbrock(x, 2)-gp.f(x),2);
+    }
+    printf("mse: %f (max squared error %f at (%f, %f) ) \n", se/N, max, xmax[0], xmax[1] );
+}
