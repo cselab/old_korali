8efa3bba88480e614aa78577ab177a619fe62876
wadaniel <wadaniel@ethz.ch>
2019-04-04 16:43:38 +0200

    clean code

--- source/TMCMC/blood/tmcmc_gp_blood.cpp
+++ source/TMCMC/blood/tmcmc_gp_blood.cpp
@@ -3,10 +3,9 @@
 int main(int argc, char** argv)
 {
 
-    Fitfun bloodgp = Fitfun(gpllk, gpllk_grad, gpllk_FIM);
+    Fitfun bloodgp = Fitfun(gpllk);
  
     TmcmcEngine engine(&bloodgp, Standard, "tmcmc_blood.par", "priors_blood.par");
-    //TmcmcEngine engine(&bloodgp, Manifold, "tmcmc_blood.par", "priors_blood.par");
     printf("run Tmcmcmc.. \n");
     engine.run();
 
