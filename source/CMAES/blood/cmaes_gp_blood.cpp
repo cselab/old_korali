ec49be08c8480b9c0a545c560637ba206d9fbce2
wadaniel <wadaniel@ethz.ch>
2019-04-12 11:43:42 +0200

    update cmaes blood

--- source/CMAES/blood/cmaes_gp_blood.cpp
+++ source/CMAES/blood/cmaes_gp_blood.cpp
@@ -42,18 +42,18 @@ int main(int argc, char** argv)
         case 6 : bds = bds_sig; break;
     }
     
-    FILE * pFile = fopen("results.txt", "a");
+    FILE * pFile = fopen(fname, "a");
     if (pFile == NULL) { printf("ERROR: could not open file!!!"); return 1; }
-    fprintf(pFile, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
-    fprintf(pFile, "XXX Profiling Parameter %d with bounds (%f, %f) and %d Steps\n", P, bds[0], bds[1], M);
-    fprintf(pFile, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
+    fprintf(pFile, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
+    fprintf(pFile, " Profiling Parameter %d with bounds (%f, %f) and %d Steps\n", P, bds[0], bds[1], M);
+    fprintf(pFile, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n");
     fclose (pFile); 
 
     for(int i = 0; i <= M; ++i) {
-
-        printf("ITERATION %d of %d \n\n", i, M);
-        double dx = i*(bds[1]-bds[0])/(double)M + bds[0];
-        auto f = [dx] (double* theta, int N, void*, int*) { double x[N+1]; for(int j = 0; j<N+1; ++j) { if ( j <P ) x[j] = theta[j]; else if (j == P) x[j] = dx; else x[j] = theta[j-1]; }  return gpllk(x, N+1); };
+        
+        double dx = (double)i*(bds[1]-bds[0])/(double)M + bds[0];
+        printf("\n\nITERATION %d of %d (dx = %.9f)\n", i, M, dx);
+        auto f = [dx] (double* theta, int N, void*, int*) { double x[N+1]; for(int j = 0; j<N+1; ++j) { if ( j < (P-1) ) x[j] = theta[j]; else if (j == (P-1)) x[j] = dx; else x[j] = theta[j-1]; }  return gpllk(x, N+1); };
         auto engine = CmaesEngine(f, "./run/");
         engine.run();
         write_best(i , engine);
@@ -70,13 +70,13 @@ void write_best(int idx, CmaesEngine& engine)
     auto evo = engine.getEvo();
     double bestFunVal = cmaes_Get(evo,"fbestever");
 
-    double dx = idx*(bds[1]-bds[0])/(double)M + bds[0];;
+    double dx = (double)idx*(bds[1]-bds[0])/(double)M + bds[0];;
 
     fprintf(pFile, "%d\t", idx);
     double x;
     for(int i = 0; i < evo->sp.N+1; ++i) {
-        if ( i < P )     x = evo->rgxbestever[i]; 
-        else if (i == P) x = dx; 
+        if ( i < (P-1) )     x = evo->rgxbestever[i]; 
+        else if (i == (P-1)) x = dx; 
         else             x = evo->rgxbestever[i-1];  
         
         fprintf(pFile, "%.15f\t", x);
